b35b8c9bb5f33691d75e295ac86b2a8a5e0bc8a6
Andy Herzog <asherzog19@gmail.com>
2017-02-01 10:40:45 -0700

    hello world

--- hello.go
+++ hello.go
@@ -2,9 +2,9 @@ package main
 
 import (
         "fmt"
-        "github.com/asherzog/string"
+    
 )
 
 func main() {
-    fmt.Println(string.Reverse("hello, world\n"))
+    fmt.Println("hello, world\n")
 }
